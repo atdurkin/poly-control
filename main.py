7e826e77d88ed55108d0a5fded0388d392e66bb8
atdurkin <alexdurkin96@gmail.com>
2025-06-05 14:02:22 +0100

    save rollout data to csv

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from utils import plot_rollouts
 
 
 env = PolyCSTR()
-df = env.generate_rollouts_with_pi_control(n_episodes=20)
+df = env.generate_rollouts_with_pi_control(n_episodes=120)
+df.to_csv("rollouts.csv", index=False)
 plot_rollouts(df)
