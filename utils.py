ac804128b11c9f0812b247c5957662670a84c5af
atdurkin <alexdurkin96@gmail.com>
2025-06-20 15:45:39 +0100

    update plot functions

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 
 class StandardScaler:
@@ -27,32 +28,94 @@ class MinMaxScaler:
         return self.min + (data - self.min_range) * (self.max - self.min) / (self.scale + 1e-8)
 
 
-def plot_rollouts(rollout_df):
-    fig, axs = plt.subplots(2, 2, figsize=(12, 8))
-
+def plot_rollouts(rollout_df, setpoints):
+ 
+    # Create a figure with a custom layout
+    fig = plt.figure(figsize=(12, 8))
+    gs = gridspec.GridSpec(3, 6)  # Define a 3x6 grid for flexibility
+ 
+    # Top row (2 subplots, centered)
+    ax1 = fig.add_subplot(gs[0, 1:3])  # First subplot spans columns 1-2
+    ax2 = fig.add_subplot(gs[0, 3:5])  # Second subplot spans columns 3-4
+ 
+    # Middle row (3 subplots)
+    ax3 = fig.add_subplot(gs[1, :2])  # First subplot spans columns 0-1
+    ax4 = fig.add_subplot(gs[1, 2:4])  # Second subplot spans columns 2-3
+    ax5 = fig.add_subplot(gs[1, 4:])  # Third subplot spans columns 4-5
+ 
+    # Bottom row (2 subplots, centered)
+    ax6 = fig.add_subplot(gs[2, 1:3])  # First subplot spans columns 1-2
+    ax7 = fig.add_subplot(gs[2, 3:5])  # Second subplot spans columns 3-4
+ 
     for ep_id, ep_data in rollout_df.groupby("episode"):
-        axs[0, 0].plot(ep_data["time"], ep_data["polymer"], alpha=0.7)
-        axs[0, 1].plot(ep_data["time"], ep_data["temperature"], alpha=0.7)
-        axs[1, 0].plot(ep_data["time"], ep_data["initiator_action"], alpha=0.7)
-        axs[1, 1].plot(ep_data["time"], ep_data["coolant_action"], alpha=0.7)
+        ax1.plot(ep_data["time"]/60, ep_data["polymer [kg/m3]"], alpha=0.7)
+        ax2.plot(ep_data["time"]/60, ep_data["temperature [K]"], alpha=0.7)
+ 
+        ax3.plot(ep_data["time"]/60, ep_data["A [kg/m3]"], alpha=0.7)
+        ax4.plot(ep_data["time"]/60, ep_data["initiator [kg/m3]"], alpha=0.7)
+        ax5.plot(ep_data["time"]/60, ep_data["radicals [mol/m3]"], alpha=0.7)
+       
+        ax6.plot(ep_data["time"]/60, ep_data["initiator feed [kg/h]"], alpha=0.7)
+        ax7.plot(ep_data["time"]/60, ep_data["coolant temp [K]"], alpha=0.7)
+ 
+    ax1.axhline(y=setpoints[0], color='k', linestyle='--')
+    ax2.axhline(y=setpoints[1], color='k', linestyle='--')
+ 
+    ax1.set_title("Polymer Concentration")
+    ax1.set_ylabel("kg/m3")
+    ax1.grid()
+ 
+    ax2.set_title("Reactor Temperature")
+    ax2.set_ylabel("K")
+    ax2.grid()
+ 
+    ax3.set_title("Monomer A Concentration")
+    ax3.set_ylabel("kg/m3")
+    ax3.grid()
+ 
+    ax4.set_title("Initiator Concentration")
+    ax4.set_ylabel("kg/m3")
+    ax4.grid()
+ 
+    ax5.set_title("Radicals Concentration")
+    ax5.set_ylabel("mol/m3")
+    ax5.grid()
+ 
+    ax6.set_title("Initiator Feed")
+    ax6.set_ylabel("kg/h")
+    ax6.grid()
+ 
+    ax7.set_title("Coolant Temperature")
+    ax7.set_ylabel("K")
+    ax7.grid()
+ 
+    ax1.set_xlabel("time [h]")
+    ax2.set_xlabel("time [h]")
+    ax3.set_xlabel("time [h]")
+    ax4.set_xlabel("time [h]")
+    ax5.set_xlabel("time [h]")
+    ax6.set_xlabel("time [h]")
+    ax7.set_xlabel("time [h]")
+ 
+    plt.tight_layout()
+    plt.show()
 
-    axs[0, 0].set_title("Polymer Concentration")
-    axs[0, 0].set_ylabel("mol/L")
-    axs[0, 0].grid()
 
-    axs[0, 1].set_title("Reactor Temperature")
-    axs[0, 1].set_ylabel("K")
-    axs[0, 1].grid()
+def plot_reward_distributions(data):
+    # data is a dictionary with keys of xtick labels and values as lists of rewards
 
-    axs[1, 0].set_title("Initiator Feed")
-    axs[1, 0].set_ylabel("mol/L")
-    axs[1, 0].set_xlabel("Time (min)")
-    axs[1, 0].grid()
+    keys = list(data.keys())
+    rewards = list(data.values())
 
-    axs[1, 1].set_title("Coolant Temperature")
-    axs[1, 1].set_ylabel("K")
-    axs[1, 1].set_xlabel("Time (min)")
-    axs[1, 1].grid()
-
+    # boxplot of the total rewards
+    plt.figure(figsize=(10, 6))
+    plt.boxplot(rewards,
+                vert=True,
+                patch_artist=True, boxprops=dict(facecolor='lightblue', color='blue'),
+                medianprops=dict(color='red'),
+                showfliers=False,)
+    plt.xticks(range(1, len(keys) + 1), keys)
+    plt.ylabel('Total episode rewards')
+    plt.grid()
     plt.tight_layout()
     plt.show()
